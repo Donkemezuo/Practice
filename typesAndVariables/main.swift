eb1b45ec6e6c03b99b83ce5745b961aa5744f40e
Donkemezuo Tariladou <DonkemezuoJnr@pursuits-macbook-pro-5.local>
2018-10-02 14:45:44 -0400

    Initial Commit

---
+++ typesAndVariables/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  typesAndVariables
+//
+//  Created by Donkemezuo Raymond Tariladou on 10/2/18.
+//  Copyright © 2018 Donkemezuo Raymond Tariladou. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
