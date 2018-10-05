8fff1c469f4ea69e5597274bdb39cdf7d666a2e7
Donkemezuo <raymonddonkemezuo.org>
2018-10-05 15:24:32 -0400

    I did it again

--- typesAndVariables/main.swift
+++ typesAndVariables/main.swift
@@ -5,8 +5,133 @@
 //  Created by Donkemezuo Raymond Tariladou on 10/2/18.
 //  Copyright © 2018 Donkemezuo Raymond Tariladou. All rights reserved.
 //
+//
+//import Foundation
+//
+//var age:integer_t = 4
+//var ageAsString: Character = "6"
+//var name = "alan"
+//print(age)
 
-import Foundation
+//let birthday = "11/19/1996"
+//print(birthday)
 
-print("Hello, World!")
+//let name = "Raymond Tari"
+//let birthday = "11/19/1996"
+//let maidenName = "Ebi"
+//let Location = "Bronx"
+//
+//print(Location)
+
+//let numberOfPeople = 34
+//let numberOfChairs = 20
+//let result = numberOfChairs <= numberOfPeople
+//print(result)
+
+//let DadsAge = 53
+//let MumsAge = 40
+//let result = DadsAge == MumsAge
+//print(result)
+//let result2 = DadsAge != MumsAge
+//print(result2)
+//
+//let result3 = DadsAge >= MumsAge
+//print(result3)
+
+
+//print(true && true)
+//print(true && false)
+//print(false && true)
+//
+//print(true || true)
+//print(true || false)
+//print(false || true)
+//print(false||false)
+//let A = 10
+//let b = 3
+//let c = 7
+//
+//print( b !=  c)
+
+
+//let isLessthan = 4 < 10
+//let isEqual = 5 == 5
+//let reverseIt = !false
+//let comboIt = !(9 > 2)
+//let isntEqual = 3 != 3
+//
+//print(isEqual)
+//print(isLessthan)
+//print(isntEqual)
+//print(reverseIt)
+//print(comboIt)
+//print(isntEqual)
+//
+//print(4 + 3 * 5)
+//print(3 / 3 * 2 + 5)
+//
+//print(2 ^ 6 - 5)
+//
+//print(5 + 4 / 2)
+//
+//print(9 - 3 * 4 - 2)
+
+//let color:Double = 4/3
+//let black:Double  = 1/2
+//let result: Int = Int (color * black)
+//print(color)
+//print(black)
+//print(result)
+
+//let num1: Double = 19
+//let num2: Double = 22
+//let result: Int = Int(num2/num1)
+//print(result)
+//let result2 = Int(num2) % Int(num1)
+//print(result2)
+
+
+
+
+//let d1 = 1.1
+//let d2 = 1.1
+//print(d1==d2)
+//print(1.2==d2+0.1)
+
+//let three = 3
+//let decimal = 0.14159
+//let pi = Double(three) + decimal
+//print(pi)
+
+//let myInt = 4
+//let myNewDouble = Double(myInt)
+//print(myNewDouble)
+//
+//let myNewInt = Int(myNewDouble)
+//print(myNewInt)
+
+
+
+//let numSeven = 4.0 + 1.2
+//let numEight = 5.0 + 0.2
+//let d = numSeven == numEight
+//print(d)
+
+
+//var i = 0
+//i = 5
+//i += 3
+//i *= 2
+//i %= 3
+//i -= 3
+//print(i)
+
+let Name = "Raymond Donkemezuo"
+let Age = 22
+let myCurrentLocation = "Bronx New York"
+
+//"my name is" + ("Name"); and I'm + ("Age")
+print("My name is \(Name) and I'm \(Age) years old")
+
+
 
